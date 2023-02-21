ce7f1bed6021d92eeca62ea9284b5d04c260a507
Alexandre Cardoso <alexandre@011TI13860.positivo.corp>
2023-02-20 23:16:09 -0300

    Added exercices from chapter 6

--- chapter05/26be2.py
+++
@@ -1,8 +0,0 @@
-"""
-Write a function, apply_to_each, that takes two arguments: a function that
-takes a single argument, and an iterable. Return a list whose values are the
-result of applying the function to each element in the iterable. (If this
-sounds familiar, it might be--this is an implementation of the classic map
-function, still available in Python. You can find a description of map in
-chapter 7.)
-"""
