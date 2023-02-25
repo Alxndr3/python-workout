7406a9a11745d74ba7233ca9599b3b20fcefca42
Alexandre Cardoso <alexandre@011TI13860.positivo.corp>
2023-02-24 21:34:24 -0300

    Added book solution for ex. 27

--- chapter06/27.py
+++ chapter06/27.py
@@ -21,3 +21,24 @@ new_pass = create_password_generator('##%%helloworld')
 print(new_pass(5))
 
 
+"""Solution to chapter 6, exercise 27: makepw"""
+
+import random
+
+
+def create_password_generator(characters):
+    """This function takes a string as input.
+
+It returns a function that, when invoked with an
+integer argument, returns a string containing
+a random selection from "characters", of length
+"length".
+"""
+    def create_password(length):
+        output = []
+
+        for i in range(length):
+            output.append(random.choice(characters))
+        return ''.join(output)
+    return create_password
+
