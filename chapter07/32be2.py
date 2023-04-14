5a8b3aa6cebe43208d52d36f74bd298b3766a385
Alexandre Cardoso <alxndr3@gmail.com>
2023-04-14 13:20:49 -0300

    Added exercice 32be2 book solution

--- chapter07/32be2.py
+++ chapter07/32be2.py
@@ -4,6 +4,7 @@ the files. The input can be a list of files from os.listdir
 
 
 from pathlib import Path
+import glob
 import os
 
 
@@ -15,6 +16,15 @@ def files_length(directory):
 def files_length_os(directory):
     return {file: f'{os.stat(file)[6]}k'
             for file in os.listdir(directory)}
+
+
+"""Solution to chapter 7, exercise 32, beyond 2: file_info"""
+
+
+def file_info(dirname):
+    return {one_filename: os.stat(one_filename).st_size
+            for one_filename in glob.glob(f'{dirname}/*')
+            if os.path.isfile(one_filename)}
 
 
 directory = Path.cwd()
